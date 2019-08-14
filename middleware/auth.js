3fddb3ba26e1f059b12ec62021dd3bc7225641e8
Ari Bambang Kurniawan <aribk210@gmail.com>
2019-08-14 10:55:41 +0700

    add login

--- middleware/auth.js
+++ middleware/auth.js
@@ -11,7 +11,7 @@ module.exports = function(req, res, next) {
   try {
     const decoded = jwt.verify(token, config.get('jwtSecret'));
 
-    req.user = user.decoded;
+    req.user = decoded.user;
     next();
   } catch (err) {
     return res.status(401).json({ msg: 'Token is not valid' });
