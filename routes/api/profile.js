d8fa01161de22f8b71934f4f8c24c7349458f619
Ari Bambang Kurniawan <aribk210@gmail.com>
2019-08-21 04:12:45 +0700

    fix bug delete all post in delete account

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -5,6 +5,7 @@ const router = express.Router();
 const auth = require('../../middleware/auth');
 const Profile = require('../../models/Profile');
 const User = require('../../models/User');
+const Post = require('../../models/Post');
 const { check, validationResult } = require('express-validator');
 // @route   GET api/profile/me
 // @desc    Get current user profile
@@ -145,6 +146,7 @@ router.get('/user/:user_id', async (req, res) => {
 // @access  Private
 router.delete('/', auth, async (req, res) => {
   try {
+    await Post.deleteMany({ user: req.user.id });
     await Profile.findOneAndRemove({ user: req.user.id });
     await User.findOneAndRemove({ _id: req.user.id });
 
